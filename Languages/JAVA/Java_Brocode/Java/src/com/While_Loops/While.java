18b411baf921c6124d1f45a8505fb81f992640ee
0xIvaR <sohamkundu4012@gmail.com>
2026-06-22 10:39:47 +0530

    Create While.java

---
+++ Languages/JAVA/Java_Brocode/Java/src/com/While_Loops/While.java
@@ -0,0 +1,14 @@
+package com.While_Loops;
+
+public class While {
+    public static void main(String[] args) {
+        double count = 15;
+
+        while (count > 0) {
+            System.out.println("T-minus " + count);
+            count--;
+        }
+
+        System.out.println("Blastoff!");
+    }
+}

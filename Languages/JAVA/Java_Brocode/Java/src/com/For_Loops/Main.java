6b2b087e49fe048e3bbe188832229f6cb37d843e
0xIvaR <sohamkundu4012@gmail.com>
2026-06-22 10:39:21 +0530

    Create Main.java

---
+++ Languages/JAVA/Java_Brocode/Java/src/com/For_Loops/Main.java
@@ -0,0 +1,14 @@
+package com.For_Loops;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // for loop = execute some code a CERTAIN amount of times
+
+        for (int count = 5; count > 0; count--) {
+            System.out.println("T-minus " + count);
+        }
+
+        System.out.println("Blastoff!");
+    }
+}

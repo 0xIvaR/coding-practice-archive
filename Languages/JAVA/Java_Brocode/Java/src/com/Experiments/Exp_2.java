a719cdd8a48de4c9e3cf231d961709f69ab7f52e
0xIvaR <sohamkundu4012@gmail.com>
2026-06-24 00:39:50 +0530

    Create Exp_2.java

---
+++ Languages/JAVA/Java_Brocode/Java/src/com/Experiments/Exp_2.java
@@ -0,0 +1,13 @@
+package com.Experiments;
+
+public class Exp_2 {
+    public static void main(String[] args) {
+
+        double number = square(3);
+        System.out.println(number);
+    }
+
+    private static double square(double i) {
+        return i * i;
+    }
+}

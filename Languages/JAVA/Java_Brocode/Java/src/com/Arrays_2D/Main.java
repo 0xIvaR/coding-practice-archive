97e5a9ba88901f1936d1c600e220e3c19061ca23
0xIvaR <sohamkundu4012@gmail.com>
2026-06-24 14:11:49 +0530

    Create Main.java

---
+++ Languages/JAVA/Java_Brocode/Java/src/com/Arrays_2D/Main.java
@@ -0,0 +1,21 @@
+package com.Arrays_2D;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 2D array = An array where each element is an array
+        //            useful for storing a matrix of data
+
+        String[] fruits = {"apple", "orange", "banana"};
+        String[] vegetables = {"potato", "onion", "carrot"};
+        String[] meats ={"chicken", "pork", "beef", "fish"};
+
+        String[][] groceries = {fruits, vegetables, meats};
+
+        for (String[] foods: groceries){
+            for (String food : foods) {
+                System.out.print(food + ", ");
+            }
+        }
+    }
+}

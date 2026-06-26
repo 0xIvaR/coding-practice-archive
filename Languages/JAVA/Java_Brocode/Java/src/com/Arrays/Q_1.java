02e92c3eeaf82ba129566c636daa3cb703d8177f
0xIvaR <sohamkundu4012@gmail.com>
2026-06-26 23:19:18 +0530

    Create Q_1.java

---
+++ Languages/JAVA/Java_Brocode/Java/src/com/Arrays/Q_1.java
@@ -0,0 +1,12 @@
+package com.Arrays;
+
+public class Q_1 {
+    public static void main(String[] args) {
+
+        int[] numericalGrades = {85, 92, 78, 64, 88};
+
+        for (int i = 0; i < numericalGrades.length; i++){
+            System.out.printf("Score Slot [%d]: %d\n", i, numericalGrades[i]);
+        }
+    }
+}

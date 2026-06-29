05a5f8f19f74b05881491087151cf05d228e2325
0xIvaR <sohamkundu4012@gmail.com>
2026-06-29 14:03:20 +0530

    Create Pattern_9.java

---
+++ Languages/JAVA/Java_Brocode/Java/src/com/Patterns/Normal_Patterns/Pattern_9.java
@@ -0,0 +1,30 @@
+package com.Patterns.Normal_Patterns;
+
+public class Pattern_9 {
+    public static void main(String[] args) {
+        // ========= PRINT THE PATTERN =========
+/*
+        // ========= OUTPUT =========
+
+            1
+            0 1
+            1 0 1
+            0 1 0 1
+            1 0 1 0 1
+
+ */
+        int num = 5;
+
+        for (int i = 1 ; i <= num; i++){
+            for (int j =1; j <= i; j++){
+                int sum = i+j;
+                if (sum % 2 == 0) { // even
+                    System.out.print("1 ");
+                } else { //odd
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}

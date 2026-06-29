e2231e6762c7478e7fa0936ff80ea8b6eaeca2f8
0xIvaR <sohamkundu4012@gmail.com>
2026-06-29 14:03:17 +0530

    Create Pattern_8.java

---
+++ Languages/JAVA/Java_Brocode/Java/src/com/Patterns/Normal_Patterns/Pattern_8.java
@@ -0,0 +1,29 @@
+package com.Patterns.Normal_Patterns;
+
+public class Pattern_8 {
+    public static void main(String[] args) {
+        // ========= FLOYD'S TRIANGLE =========
+/*
+        // ========= OUTPUT =========
+
+            1
+            2 3
+            4 5 6
+            7 8 9 10
+            11 12 13 14 15
+
+ */
+        int num = 5;
+        int number = 1;
+
+        // outer loop
+        for (int i = 1; i <= num; i++){
+            // inner loop
+            for (int j =1; j <= i; j++){
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
